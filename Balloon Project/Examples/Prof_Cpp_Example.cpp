c1b5bb9c79ed50d73d37dfb154433cd38c5964cc
FryerWire <maxwell.seery@gmail.com>
2025-04-07 22:26:32 -0500

    Minor changes

--- Balloon Project/Examples/Prof_Cpp_Example.cpp
+++ Balloon Project/Examples/Prof_Cpp_Example.cpp
@@ -98,3 +98,61 @@ void loop() {
 //   Serial.println("Wrote new measurement to data file!");
 
 //  }
+
+
+
+
+
+#include <Arduino.h>
+#include "Adafruit_CircuitPlayground.h" // Local project library
+
+
+// Runs about 45 MHz
+// Build is compile 
+// RAM: [==   ] 16%
+// Flash: [= ] 14% Program code and not data file. 
+
+
+
+void setup() {
+  Serial.begin(115200); // 115200 bits per second
+  CircuitPlayground.begin(); // Initialize the Circuit Playground board
+}
+
+
+
+void loop() {
+  CircuitPlayground.redLED(HIGH);
+  delay(500); // Blocking technique, this does not allow other code to run
+  // Can use timers to do this without blocking
+  CircuitPlayground.redLED(LOW);
+  delay(500); // Blocking technique, this does not allow other code to run
+  Serial.print("HelloWorld!"); // Prints
+
+  float X = CircuitPlayground.motionX();
+  Serial.print("Motion X: ");
+  Serial.println(X);
+}
+
+
+
+
+
+
+// // put function declarations here:
+// int myFunction(int, int);
+
+// void setup() {
+//   // put your setup code here, to run once:
+//   // int result = myFunction(2, 3);
+//   CircuitPlayground.begin();
+// }
+
+// void loop() {
+//   // put your main code here, to run repeatedly:
+// }
+
+// // put function definitions here:
+// int myFunction(int x, int y) {
+//   return x + y;
+// }
--- Balloon Project/src/main.cpp
+++ Balloon Project/src/main.cpp
@@ -1,54 +1,14 @@
+// /*
+// main.cpp
+// Start Date : 4/7/2025
+// Modified   : 4/7/2025
+// */
 
 #include <Arduino.h>
-#include "Adafruit_CircuitPlayground.h" // Local project library
 
+int a = 10;       // Integer
 
-// Runs about 45 MHz
-// Build is compile 
-// RAM: [==   ] 16%
-// Flash: [= ] 14% Program code and not data file. 
-
-
-
-void setup() {
-  Serial.begin(115200); // 115200 bits per second
-  CircuitPlayground.begin(); // Initialize the Circuit Playground board
-}
-
-
-
-void loop() {
-  CircuitPlayground.redLED(HIGH);
-  delay(500); // Blocking technique, this does not allow other code to run
-  // Can use timers to do this without blocking
-  CircuitPlayground.redLED(LOW);
-  delay(500); // Blocking technique, this does not allow other code to run
-  Serial.print("HelloWorld!"); // Prints
-
-  float X = CircuitPlayground.motionX();
-  Serial.print("Motion X: ");
-  Serial.println(X);
-}
-
-
-
-
-
-
-// // put function declarations here:
-// int myFunction(int, int);
-
-// void setup() {
-//   // put your setup code here, to run once:
-//   // int result = myFunction(2, 3);
-//   CircuitPlayground.begin();
-// }
-
-// void loop() {
-//   // put your main code here, to run repeatedly:
-// }
-
-// // put function definitions here:
-// int myFunction(int x, int y) {
-//   return x + y;
-// }
+[env:arduino]
+platform = atmelavr
+board = uno
+framework = arduino
